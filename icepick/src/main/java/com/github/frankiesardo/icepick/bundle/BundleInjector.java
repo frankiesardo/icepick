6b26d917a8b38f140c392e7e884fbda540581d45
frankiesardo <fran.sardo@gmail.com>
2013-08-02 17:34:21 +0100

    Refactor injector duplication using an abstract superclass

--- icepick/src/main/java/com/github/frankiesardo/icepick/bundle/BundleInjector.java
+++ icepick/src/main/java/com/github/frankiesardo/icepick/bundle/BundleInjector.java
@@ -2,64 +2,31 @@ package com.github.frankiesardo.icepick.bundle;
 
 import android.os.Bundle;
 
-import com.github.frankiesardo.icepick.annotation.IcicleProcessor;
-
 import java.lang.reflect.Method;
 import java.util.Map;
 
-class BundleInjector {
+class BundleInjector extends AbstractBundleInjector<Bundle> {
 
-    static final Method NO_OP = null;
-
-    private final Object target;
-    private final Bundle bundle;
-    private final Map<BundleMethodKey, Method> cachedMethods;
-
-    public BundleInjector(Object target, Bundle bundle, Map<BundleMethodKey, Method> cachedMethods) {
-        this.target = target;
-        this.bundle = bundle;
-        this.cachedMethods = cachedMethods;
+    BundleInjector(Object target, Bundle argument, Map<BundleMethodKey, Method> cachedMethods) {
+        super(target, argument, cachedMethods);
     }
 
-    public void inject(BundleAction action) {
+    void inject(BundleAction action) {
         Class<?> targetClass = target.getClass();
         try {
             Method inject = getMethodFromHelper(targetClass, action);
             if (inject != null) {
-                inject.invoke(null, target, bundle);
+                inject.invoke(null, target, argument);
             }
         } catch (RuntimeException e) {
             throw e;
         } catch (Exception e) {
-            throw new UnableToInjectException("Unable to manipulate bundle for " + target, e);
+            throw new UnableToInjectException(target, e);
         }
     }
 
-    private Method getMethodFromHelper(Class<?> cls, BundleAction bundleAction) throws NoSuchMethodException {
-        BundleMethodKey bundleMethodKey = new BundleMethodKey(cls, bundleAction);
-        Method method = cachedMethods.get(bundleMethodKey);
-        if (method != null) {
-            return method;
-        }
-
-        String clsName = cls.getName();
-        if (clsName.startsWith("android.") || clsName.startsWith("java.")) {
-            return NO_OP;
-        }
-
-        try {
-            Class<?> helper = Class.forName(clsName + IcicleProcessor.SUFFIX);
-            method = helper.getMethod(bundleAction.name, cls, Bundle.class);
-        } catch (ClassNotFoundException e) {
-            method = getMethodFromHelper(cls.getSuperclass(), bundleAction);
-        }
-        cachedMethods.put(bundleMethodKey, method);
-        return method;
-    }
-
-    public static class UnableToInjectException extends RuntimeException {
-        UnableToInjectException(String message, Throwable cause) {
-            super(message, cause);
-        }
+    @Override
+    protected Class<?> getArgumentClass() {
+        return Bundle.class;
     }
 }
