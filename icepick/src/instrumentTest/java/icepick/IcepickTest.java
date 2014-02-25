ad314e6bd1eed6e631303a3ea64176ffa3a0bfe4
frankiesardo <fran.sardo@gmail.com>
2014-02-26 00:09:17 +0100

    Remove autowrapping ability from Icepick

--- icepick/src/instrumentTest/java/icepick/IcepickTest.java
+++ icepick/src/instrumentTest/java/icepick/IcepickTest.java
@@ -1,51 +1,12 @@
 package icepick;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.test.AndroidTestCase;
-import java.io.File;
-import java.io.Serializable;
-import java.util.Arrays;
-import java.util.List;
 
 import static icepick.Icepick.restoreInstanceState;
 import static icepick.Icepick.saveInstanceState;
-import static icepick.Icepick.unwrap;
-import static icepick.Icepick.wrap;
 
 public class IcepickTest extends AndroidTestCase {
-
-  public void testWrapAndUnwrap() throws Exception {
-    String string = "string";
-    int primitive = Integer.MAX_VALUE;
-    Object object = Action.SAVE;
-    Serializable serializable = new File("some/path");
-    Parcelable parcelable = Uri.parse("http://google.com");
-    List<String> arrayList = Arrays.asList("one", "two", "three");
-
-    assertIdempotent(string);
-    assertIdempotent(primitive);
-    assertIdempotent(object);
-    assertIdempotent(serializable);
-    assertIdempotent(parcelable);
-    assertIdempotent(arrayList);
-  }
-
-  public void testThrowsExceptionForNonWrappableInstances() throws Exception {
-    try {
-      assertIdempotent(new Bundle());
-      fail();
-    } catch(Icepick.UnableToSerializeException e) {}
-  }
-
-  public void testNullSafe() throws Exception {
-    assertIdempotent(null);
-  }
-
-  private void assertIdempotent(Object o) {
-    assertEquals(o, unwrap(wrap(o)));
-  }
 
   public void testSaveAndRestoreInstanceState() throws Exception {
     ClassToInject classToInject = new ClassToInject();
