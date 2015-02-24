5ec6eb3fa5107adc33eba356c45f70cb5c31046c
frankiesardo <fran.sardo@gmail.com>
2015-02-24 23:29:18 +0000

    Automatic snapshots

--- project.clj
+++ project.clj
@@ -2,4 +2,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-sub "0.3.0"]]
+  :deploy-repositories {"snapshots" {:url "https://clojars.org/repo"
+                                     :username [:gpg :env/clojars_username]
+                                     :password [:gpg :env/clojars_password]}}
   :sub ["icepick" "icepick-processor"])
