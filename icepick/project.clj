d471a022b0f72634d603cf80fe50779f910e1fe2
Frankie Sardo <fran.sardo@gmail.com>
2015-03-12 21:16:37 +0000

    Release 3.0.2

--- icepick/project.clj
+++ icepick/project.clj
@@ -1,4 +1,4 @@
-(defproject frankiesardo/icepick "3.0.2-SNAPSHOT"
+(defproject frankiesardo/icepick "3.0.2"
   :description "Android instance state made easy"
   :url "https://github.com/frankiesardo/icepick"
   :license {:name "Eclipse Public License"
