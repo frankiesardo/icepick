d7affd359b364ad1914cc78d190deb1d767deffc
Frankie Sardo <fran.sardo@gmail.com>
2015-07-23 01:30:01 +0100

    Version 3.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.frankiesardo/icepick-parent "3.1.0"
+(defproject com.github.frankiesardo/icepick-parent "3.1.1-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-sub "0.3.0"]]
