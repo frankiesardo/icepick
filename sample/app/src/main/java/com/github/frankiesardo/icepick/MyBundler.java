f97cfb6815ebecfc1e0f072b6f1d723d684d72ca
Jonathan Caryl <github@jonathancaryl.co.uk>
2017-03-08 09:43:08 +0000

    Update sample app

--- sample/app/src/main/java/com/github/frankiesardo/icepick/MyBundler.java
+++ sample/app/src/main/java/com/github/frankiesardo/icepick/MyBundler.java
@@ -4,14 +4,15 @@ import android.os.Bundle;
 
 import icepick.Bundler;
 
-public class MyBundler implements Bundler<String> {
+class MyBundler implements Bundler<String> {
     @Override
     public void put(String key, String value, Bundle bundle) {
         bundle.putString(key, value + "*");
     }
 
     @Override
-    public <V extends String> V get(String key, Bundle bundle) {
-        return (V) bundle.getString(key);
+    public String get(String key, Bundle bundle) {
+        return bundle.getString(key);
     }
+
 }
