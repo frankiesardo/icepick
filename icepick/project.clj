7a3fb08f8ef00cc5622c5ca41fdf711f888b633f
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-13 20:55:59 +0900

    Fix license name

--- icepick/project.clj
+++ icepick/project.clj
@@ -1,7 +1,7 @@
 (defproject frankiesardo/icepick "3.2.1-SNAPSHOT"
   :description "Android instance state made easy"
   :url "https://github.com/frankiesardo/icepick"
-  :license {:name "Eclipse Public License"
+  :license {:name "Eclipse Public License -v 1.0"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["src"]
   :javac-options ["-target" "1.6" "-source" "1.6"]
--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.github.frankiesardo/icepick-parent "3.2.1-SNAPSHOT"
-  :license {:name "Eclipse Public License"
+  :license {:name "Eclipse Public License -v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-sub "0.3.0"]]
   :sub ["icepick" "icepick-processor"])
